e23d42086cd038dc92a31f192faf85057346fede
atar1boy <atar1boy@yandex.ru>
2023-06-26 20:00:44 +0300

    Полностью написаны модели для приложения recipes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Tags, Ingredients
+
+from .models import (
+    Tags, Ingredients, IngredientsInRecipe,
+    Recipe, Favourites, ShoppingCart
+)
 
 
 admin.site.register(Tags)
 admin.site.register(Ingredients)
+admin.site.register(IngredientsInRecipe)
+admin.site.register(Recipe)
+admin.site.register(Favourites)
+admin.site.register(ShoppingCart)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,5 +1,8 @@
 from colorfield.fields import ColorField
 from django.db import models
+from django.core.validators import MinValueValidator
+
+from users.models import CustomUser
 
 
 class Tags(models.Model):
@@ -49,3 +52,130 @@ class Ingredients(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """
+    Модель рецептов, включающая в себя поля связанные many-to-many модели:
+    Ingredients и Tags. Модель принимает изображения в Base64 кодировке.
+    """
+    author = models.ForeignKey(
+        CustomUser,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        verbose_name='Ингредиенты блюда',
+        through='IngredientsInRecipe',
+    )
+    tags = models.ManyToManyField(
+        Tags,
+        verbose_name='Теги блюда',
+    )
+    image = models.ImageField(
+        'Изображение рецепта',
+        upload_to='recipe',
+        blank=True
+    )
+    name = models.CharField(
+        'Название рецепта',
+        max_length=200,
+    )
+    text = models.TextField(
+        'Описание рецепта'
+    )
+    cooking_time = models.IntegerField(
+        'Время пригтовления',
+        validators=[MinValueValidator(
+            1,
+            ('Неправильное значение cooking_time '
+             'Это поле должно содержать число больше '
+             'или равное "1"')
+        )]
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class AbstractRecipeModel(models.Model):
+    """Абстрактная Модель"""
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class IngredientsInRecipe(AbstractRecipeModel):
+    """
+    Сквозная модель хранящая ингредиенты и их количество в рецепте.
+    """
+    ingredients = models.ForeignKey(
+        Ingredients,
+        verbose_name='Ингредиент в рецепте',
+        on_delete=models.CASCADE,
+    )
+    amount = models.FloatField(
+        'Количество ингредиента числом',
+        validators=[MinValueValidator(
+            0,
+            ('Неправильное значение amount '
+             'Это поле должно содержать число больше "0"')
+        )]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецепте'
+
+    def __str__(self):
+        return f'{self.ingredients} в {self.recipe}'
+
+
+class Favourites(AbstractRecipeModel):
+    """
+    Модель Избранных рецептов.
+    """
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='favourites',
+    )
+
+    class Meta():
+        unique_together = ('user', 'recipe',)
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные рецепты'
+
+    def __str__(self):
+        return f'{self.recipe} в избранном у {self.user}'
+
+
+class ShoppingCart(AbstractRecipeModel):
+    """
+    Модель Корзины Покупок.
+    """
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta():
+        unique_together = ('user', 'recipe',)
+        verbose_name = 'Корзина покупок'
+        verbose_name_plural = 'Корзина покупок'
+
+    def __str__(self):
+        return f'{self.recipe} в корзине у {self.user}'
