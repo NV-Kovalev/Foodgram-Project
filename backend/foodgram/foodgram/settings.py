e4db94812755b53d39ea49b2d6263bcb232c6c40
atar1boy <atar1boy@yandex.ru>
2023-07-03 03:00:35 +0300

    Изменен DEBUG на False по дефолту

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['localhost', 'backend', '127.0.0.1', 'django']
 CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://*.127.0.0.1']
